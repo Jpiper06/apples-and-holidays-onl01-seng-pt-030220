c91376de80e28841b4d8168b61ba4e56d01dc285
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-15 21:56:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,9 +29,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 end
 
 
-
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash [:spring] [:memorial_day] << ["BBQ"] 
+  holiday_hash [:spring] [:memorial_day] << supply  
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
